3a7929058a2fe1ed2a243126b708aa8fbfd191e2
galba-rp <yaroslav.hontar@gmail.com>
2021-04-09 15:42:31 +0200

    refacto: correct css path for heroku

--- layouts/header.php
+++ layouts/header.php
@@ -10,7 +10,7 @@ $current=basename($path);
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="../styles/style.css">
+  <link rel="stylesheet" href="/../styles/style.css">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
     integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
   <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
