f93cc07941fce0c86f1fb0908d64e7123d9a357e
nicott35 <78737687+nicott35@users.noreply.github.com>
2021-04-12 08:14:58 +0200

    Update 6.php

--- pages/6.php
+++ pages/6.php
@@ -1,33 +1,30 @@
-<?php 
-$page="Maison";
-include('../layouts/header.php') ?> <main id="apropos" class="pad-top">
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN" "http://www.w3.org/TR/REC-html40/loose.dtd">
+<?php $page="Maison";
+include('../layouts/header.php') ?><html><body>
+<main id="apropos" class="pad-top">
     <section class="container">
-        <h1 class="section-header">Title de l'annonce</h1>
+        <h1 class="section-header">maison / villa 5 pi&egrave;ces &agrave; Questembert</h1>
         <div class="d-flex" id="secion-ad">
-            <img src="../images/actuel_2_chambres.jpg" alt="maison" class="img-fluid mr-3">
+            <img src="../images/0ygzhyhqfrsapy4ccjxpdtfcbj1i2spxqa3snkyrk.webp" alt="maison" class="img-fluid mr-3">
             <div class="d-flex flex-column" id="single-ad-description">
                 <div class="d-flex justify-content-beetween">
-                    <p><img src="../images/meters.png" class="img-fluid" alt="surface"> 100 m²</p>
-                    <p><img src="../images/localisation.png" class="img-fluid" alt="surface"> Ville 00000</p>
-                    <p><img src="../images/price.png" class="img-fluid" alt="surface"> Prix €*</p>
-                    <p><img src="../images/energy.png" class="img-fluid" alt="surface"> Cl Energ</p>
-                    <p><img src="../images/calendar.png" class="img-fluid" alt="surface"> Année</p>
+                    <p><img src="../images/meters.png" class="img-fluid" alt="surface">95 m&sup2;</p>
+                       <p><img src="../images/localisation.png" class="img-fluid" alt="surface">Questembert</p>
+                    <p><img src="../images/price.png" class="img-fluid" alt="price">189 500 &euro;</p>
+                    <p><img src="../images/energy.png" class="img-fluid" alt="classene">E</p>
+                    <p><img src="../images/calendar.png" class="img-fluid" alt="annee">1960</p>
                 </div>
-                <p>
-
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam pretium sit amet enim vitae maximus.
-                    Etiam fermentum auctor ipsum ut lobortis. Suspendisse potenti. Cras luctus sodales risus nec
-                    molestie. Sed id ante efficitur, ultricies enim ac, tincidunt sem. Phasellus iaculis molestie dui,
-                    sed posuere arcu. In hac habitasse platea dictumst. Etiam molestie efficitur nisl.
-
-                    Cras in lacus sagittis, rutrum mi ac, dictum libero. Donec venenatis ex odio, ut dapibus risus
-                    congue ut. Nam dignissim erat et ipsum sodales convallis. Suspendisse nisi neque, bibendum consequat
-                    dolor vitae, ultrices bibendum dui. Maecenas vulputate nunc sit amet neque posuere, non efficitur
-                    arcu dignissim. Maecenas id felis euismod, sodales ante eget, varius lacus. Nullam felis turpis,
-                    iaculis ut quam sed, auctor porttitor diam. Integer ipsum dui, semper ac auctor et, scelerisque sed
-                    arcu. </p>
-                <p> * Le prix indiqué [ne] comprend [pas] les honoraires à la charge de l'acheteur</p>
+                <p>2
+ mins de la gare, 25 mins de Vannes et 20 mins des plages. Situation 
+id&eacute;ale pour cette traditionnelle sur sous-sol r&eacute;nov&eacute;e. Elle offre en 
+rez-de-chauss&eacute;e sur&eacute;lev&eacute; une pi&egrave;ce de vie avec cuisine am&eacute;nag&eacute;e ouverte,
+ 2 chambres, salle d'eau, WC. A l'&eacute;tage: un d&eacute;gagement dessert 2 
+chambres, salle de bain, WC. Garage, atelier. Vous profiterez d'un 
+jardin clos de 500m&sup2; environ. Id&eacute;al 1&egrave;re acquisition ou projet locatif !
+ Dont 5.28 % honoraires TTC &agrave; la charge de l'acqu&eacute;reur.</p>
+                <p> * Le prix indiqué comprend les honoraires à la charge de l'acheteur</p>
             </div>
         </div>
     </section>
 </main> <?php include('../layouts/footer.php') ?>
+</body></html>
