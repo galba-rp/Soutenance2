9361fb8e15fd15d15a50b16b3fa1ee2ff24fa4c0
nicott35 <78737687+nicott35@users.noreply.github.com>
2021-04-12 08:18:35 +0200

    Update 10.php

--- pages/10.php
+++ pages/10.php
@@ -1,33 +1,30 @@
-<?php 
-$page="Maison";
-include('../layouts/header.php') ?> <main id="apropos" class="pad-top">
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN" "http://www.w3.org/TR/REC-html40/loose.dtd">
+<?php $page="Maison";
+include('../layouts/header.php') ?><html><body>
+<main id="apropos" class="pad-top">
     <section class="container">
-        <h1 class="section-header">Title de l'annonce</h1>
+        <h1 class="section-header">maison / villa 6 pi&egrave;ces &agrave; Questembert</h1>
         <div class="d-flex" id="secion-ad">
-            <img src="../images/actuel_2_chambres.jpg" alt="maison" class="img-fluid mr-3">
+            <img src="../images/0lyo04m6n73ztiif0vbz6xupxvknslaqi7lspr3fk.webp" alt="maison" class="img-fluid mr-3">
             <div class="d-flex flex-column" id="single-ad-description">
                 <div class="d-flex justify-content-beetween">
-                    <p><img src="../images/meters.png" class="img-fluid" alt="surface"> 100 m²</p>
-                    <p><img src="../images/localisation.png" class="img-fluid" alt="surface"> Ville 00000</p>
-                    <p><img src="../images/price.png" class="img-fluid" alt="surface"> Prix €*</p>
-                    <p><img src="../images/energy.png" class="img-fluid" alt="surface"> Cl Energ</p>
-                    <p><img src="../images/calendar.png" class="img-fluid" alt="surface"> Année</p>
+                    <p><img src="../images/meters.png" class="img-fluid" alt="surface"> 100 m&sup2;</p>
+                    <p><img src="../images/localisation.png" class="img-fluid" alt="surface">Questembert</p>
+                    <p><img src="../images/price.png" class="img-fluid" alt="surface">244 400 &euro;</p>
+                    <p><img src="../images/energy.png" class="img-fluid" alt="surface">E</p>
+                    <p><img src="../images/calendar.png" class="img-fluid" alt="surface">1978</p>
                 </div>
-                <p>
-
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam pretium sit amet enim vitae maximus.
-                    Etiam fermentum auctor ipsum ut lobortis. Suspendisse potenti. Cras luctus sodales risus nec
-                    molestie. Sed id ante efficitur, ultricies enim ac, tincidunt sem. Phasellus iaculis molestie dui,
-                    sed posuere arcu. In hac habitasse platea dictumst. Etiam molestie efficitur nisl.
-
-                    Cras in lacus sagittis, rutrum mi ac, dictum libero. Donec venenatis ex odio, ut dapibus risus
-                    congue ut. Nam dignissim erat et ipsum sodales convallis. Suspendisse nisi neque, bibendum consequat
-                    dolor vitae, ultrices bibendum dui. Maecenas vulputate nunc sit amet neque posuere, non efficitur
-                    arcu dignissim. Maecenas id felis euismod, sodales ante eget, varius lacus. Nullam felis turpis,
-                    iaculis ut quam sed, auctor porttitor diam. Integer ipsum dui, semper ac auctor et, scelerisque sed
-                    arcu. </p>
-                <p> * Le prix indiqué [ne] comprend [pas] les honoraires à la charge de l'acheteur</p>
+                <p>ville
+ dynamique avec toutes les commodit&eacute;s. Situ&eacute;e dans un lotissement tr&egrave;s 
+calme sur un terrain de 647 m2 environ, cette maison de 126 m2 saura 
+vous s&eacute;duire par ses volumes. Elle est compos&eacute;e au rdc d'une vaste 
+cuisine de 19 m2 environ ouverte sur un s&eacute;jour de 22m2 agr&eacute;ment&eacute; d'un 
+po&ecirc;le a bois, d'une chambre de 13 m2, d'une salle d'eau avec wc et d'un 
+cellier. Au 1er &eacute;tage 3 chambres, 1 pi&egrave;ce pouvant faire office de 
+bureau, 1 salle d'eau et 1 wc</p>
+                <p> * Le prix indiqué comprend les honoraires à la charge de l'acheteur</p>
             </div>
         </div>
     </section>
 </main> <?php include('../layouts/footer.php') ?>
+</body></html>
